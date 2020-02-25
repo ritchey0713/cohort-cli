bed8fd9fbff640f17b9825807e55a7b127d392e3
ritchey0713 <ritchey0713@gmail.com>
2020-02-25 18:23:15 -0500

    add cli file, require cli file

--- lib/cohortCLI.rb
+++ lib/cohortCLI.rb
@@ -2,6 +2,7 @@
 # in here loads all files needed to run our app
 
 require "cohortCLI/version"
+require "cohortCLI/cli"
 
 module CohortCLI
   class Error < StandardError; end
---
+++ lib/cohortCLI/cli.rb
@@ -0,0 +1,8 @@
+class CohortCli::CLI 
+  
+  def start 
+    puts "Hello there!"
+  end
+
+end
+  
